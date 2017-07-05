2bc4e1afd6b374f1f53f47d8842e4412299e897c
Clifton King <cliftonk@gmail.com>
2017-07-05 16:52:07 -0400

    airflow (#109)
    * local airflow working
    
    * add airflow to infrastructure base build
    
    * conflicting flask versions
    
    * start of kube deployed airflow webserver
    
    * add nslookup to base container
    
    * install airflow last
    
    * combine kube airflow spec into 1 file
    
    * add airflow worker and scheduler to kube
    
    * fixed redis error from console page @maximilianr
    
    * need newer airflow version bc of gbq
    
    * use upgrade strategy 'only if needed' for deps
    
    * dags folder configurable via env var
    
    * remove loading examples from cfg
    
    * add airflow setup file to create connections
    
    * add setup.py
    
    * use airflow-local as entrypoint
    
    * rename to airflow-entry.sh
    
    * alter path to entrypoint

---
+++ docker/airflow/airflow-entry.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+AIRFLOW_HOME="/usr/local/airflow"
+CMD="/usr/local/bin/airflow"
+TRY_LOOP="20"
+
+: ${REDIS_HOST:="redis"}
+: ${REDIS_PORT:="6379"}
+: ${REDIS_PASSWORD:=""}
+
+#
+# : ${POSTGRES_HOST:="postgres"}
+: ${POSTGRES_PORT:="5432"}
+# : ${POSTGRES_USER:="airflow"}
+# : ${POSTGRES_PASSWORD:="airflow"}
+# : ${POSTGRES_DB:="airflow"}
+#
+# : ${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}
+
+# Wait for Postresql
+if [ "$1" = "webserver" ] || [ "$1" = "worker" ] || [ "$1" = "scheduler" ] ; then
+  i=0
+  while ! nc -z $POSTGRES_HOST $POSTGRES_PORT >/dev/null 2>&1 < /dev/null; do
+    i=$((i+1))
+    if [ "$1" = "webserver" ]; then
+      echo "$(date) - waiting for ${POSTGRES_HOST}:${POSTGRES_PORT}... $i/$TRY_LOOP"
+      if [ $i -ge $TRY_LOOP ]; then
+        echo "$(date) - ${POSTGRES_HOST}:${POSTGRES_PORT} still not reachable, giving up"
+        exit 1
+      fi
+    fi
+    sleep 10
+  done
+fi
+
+# Wait for Redis
+if [ "$1" = "webserver" ] || [ "$1" = "worker" ] || [ "$1" = "scheduler" ] || [ "$1" = "flower" ] ; then
+  j=0
+  while ! nc -z $REDIS_HOST $REDIS_PORT >/dev/null 2>&1 < /dev/null; do
+    j=$((j+1))
+    if [ $j -ge $TRY_LOOP ]; then
+      echo "$(date) - $REDIS_HOST still not reachable, giving up"
+      exit 1
+    fi
+    echo "$(date) - waiting for Redis... $j/$TRY_LOOP"
+    sleep 5
+  done
+fi
+
+echo "Initialize database..."
+$CMD initdb
+python ${AIRFLOW_HOME}/airflow_setup.py
+
+$CMD "$@"
