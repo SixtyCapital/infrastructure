a2048c1b7fbaf8dd383d5c030fe358a9f5afb52b
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2019-03-06 15:29:04 -0500

    remove some old configs (#264)

--- sixtyctl/config.py
+++ sixtyctl/config.py
@@ -1,16 +1,13 @@
 SCRATCH_PROJECTS = [
     'sixty-capital',
-    ]
+]
 STRATEGY_PROJECTS = [
     'sixty-secure',
     'sixty-commodity',
-    'sixty-capital-similarity',
     'sixty-capital-nursery',
-    ]
+]
 EXTERNAL_PROJECTS = [
     'sixty-capital-ext',
-    ]
+]
 KMS_PROJECT = 'sixty-capital'
 PRODUCTION_PROJECT = 'sixty-capital-prod'
-DEFAULT_CONFIG = 'config_airflow.yaml'
-PRODUCTION_CONFIG = 'config_airflow_production.yaml'
