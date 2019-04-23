096d99caff76a43626602b1abf665daf8e1f850b
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2019-04-23 09:40:25 -0400

    fix for starting airflow (#295)
    * fix for starting airflow
    
    * note

--- docker/airflow/airflow-init.sh
+++ docker/airflow/airflow-init.sh
@@ -4,51 +4,41 @@
 # from https://github.com/mumoshu/kube-airflow/blob/master/script/entrypoint.sh
 
 set -e
-AIRFLOW_HOME="/usr/local/airflow"
-CMD="/usr/local/bin/airflow"
 TRY_LOOP="20"
 
+# we use celery redis but this only sets if not externally set
 : ${REDIS_HOST:="redis"}
 : ${REDIS_PORT:="6379"}
 : ${REDIS_PASSWORD:=""}
 
-#
 : ${POSTGRES_HOST:="postgres"}
 : ${POSTGRES_PORT:="5432"}
 
 # Wait for Postresql
-if [ "$1" = "initdb" ] || [ "$1" = "webserver" ] || [ "$1" = "worker" ] || [ "$1" = "scheduler" ] ; then
-  i=0
-  while ! nc -z $POSTGRES_HOST $POSTGRES_PORT >/dev/null 2>&1 < /dev/null; do
-    i=$((i+1))
-    echo "$(date) - waiting for ${POSTGRES_HOST}:${POSTGRES_PORT}... $i/$TRY_LOOP"
-    if [ $i -ge $TRY_LOOP ] ; then
-      echo "$(date) - ${POSTGRES_HOST}:${POSTGRES_PORT} still not reachable, giving up"
-      exit 1
-    fi
-    sleep 5
-  done
-  echo "Initialize database..."
-  $CMD initdb
-  # don't do anything else if cmd is initdb
-  if [ "$1" = "initdb" ] ; then
-    exit 0
+i=0
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT >/dev/null 2>&1 < /dev/null; do
+  i=$((i+1))
+  echo "$(date) - waiting for ${POSTGRES_HOST}:${POSTGRES_PORT}... $i/$TRY_LOOP"
+  if [ $i -ge $TRY_LOOP ] ; then
+    echo "$(date) - ${POSTGRES_HOST}:${POSTGRES_PORT} still not reachable, giving up"
+    exit 1
   fi
-fi
+  sleep 5
+done
+echo "Initialize database..."
+airflow initdb
 
 # Wait for Redis
 
 # don't wait for redis if you don't need to (this is dependent on executor being set with an env. Possible to set with
 # other config settings, and then this process won't recognize)
-if [ "$1" = "webserver" ] || [ "$1" = "worker" ] || [ "$1" = "scheduler" ] || [ "$1" = "flower" ] ; then
-  j=0
-  while ! nc -z $REDIS_HOST $REDIS_PORT >/dev/null 2>&1 < /dev/null; do
-    j=$((j+1))
-    if [ $j -ge $TRY_LOOP ]; then
-      echo "$(date) - $REDIS_HOST still not reachable, giving up"
-      exit 1
-    fi
-    echo "$(date) - waiting for Redis... $j/$TRY_LOOP"
-    sleep 5
-  done
-fi
+j=0
+while ! nc -z $REDIS_HOST $REDIS_PORT >/dev/null 2>&1 < /dev/null; do
+  j=$((j+1))
+  if [ $j -ge $TRY_LOOP ]; then
+    echo "$(date) - $REDIS_HOST still not reachable, giving up"
+    exit 1
+  fi
+  echo "$(date) - waiting for Redis... $j/$TRY_LOOP"
+  sleep 5
+done
